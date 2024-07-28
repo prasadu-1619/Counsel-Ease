c45378d633f0bba2c29f5dd3f49abaacd34c30e6
PRASAD KISHOR UMBARKAR <160735725+prasadu-1619@users.noreply.github.com>
2024-07-28 23:43:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pyotp
 import datetime
 import time
 import os
+import signal
 import sys
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
